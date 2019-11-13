3220fb061ff50e01ed25245ca0a3ae02588700af
danoan <danoan2008@gmail.com>
2019-11-13 12:55:33 +0100

    Remove of setUp.sh

--- setUp.sh
+++
@@ -1,9 +0,0 @@
-#List of directories in which cmake will look up
-#for packages by means of find_package utility.
-#find_package is called for DGtal and openCV
-#libraries
-FIND_PACKAGE_EXTRA_LOOK_UP_DIRS=$1
-BOOST_LIBS_DIR=$2
-
-sh build-scripts/clone-repos.sh
-sh build-scripts/make-ext-project.sh $FIND_PACKAGE_EXTRA_LOOK_UP_DIRS $BOOST_LIBS_DIR release
