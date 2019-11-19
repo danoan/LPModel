8f02f36ee3b90be47d1b66a3f98ab03bba84476c
danoan <danoan2008@gmail.com>
2019-11-19 17:17:06 +0100

    Remove of debug variables

--- modules/terms/src/sqc/adapters/linel/control/IteratorManager.cpp
+++ modules/terms/src/sqc/adapters/linel/control/IteratorManager.cpp
@@ -7,19 +7,12 @@ void IteratorManager::run(const Parameters &prm, const BinaryCallback &bcbk, con
     DGtal::Z2i::DigitalSet temp(prm.odrModel.domain);
     DIPaCUS::Misc::DigitalBallIntersection DBIOptimization = prm.handle.intersectionComputer(prm.odrModel.optRegion);
 
-    double bSum=0;
-    double tSum=0;
-
-
     for(auto linelPtr=prm.odrModel.applicationRegion.begin();linelPtr!=prm.odrModel.applicationRegion.end();++linelPtr)
     {
         DGtal::Z2i::Point linel = *linelPtr;
 
         temp.clear();
         DBIOptimization(temp, linel);
-
-        tSum += (temp.size()*(temp.size()-1))/2;
-        bSum += temp.size();
 
         for (auto pit = temp.begin(); pit != temp.end(); ++pit)
         {
